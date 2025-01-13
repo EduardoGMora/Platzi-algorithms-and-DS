8468680bb831d29ef403b3309321283f8a908cf0
EduardoGMora <lalogomo2000@outlook.com>
2025-01-13 12:59:54 -0600

    insertion sort implemetation

---
+++ sort algorithms/insertionsort.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <cstring>
+#include <math.h>
+
+using namespace std;
+
+int print(int arr[], int n){
+    for(int i = 0; i < n; i++)
+        cout << arr[i] << " ";
+    cout << endl;
+
+    return 0;
+}
+
+void insertionSort(int arr[], int n){
+    int currentvalue, i, j;
+    for(i=1; i<n; i++){
+        currentvalue = arr[i];
+        j = i-1;
+
+        while(j>=0 && arr[j]>currentvalue){
+            arr[j+1] = arr[j];
+            j = j-1;
+        }
+    }
+        
+}
+
+
+int main(){
+    int arr[] = {64, 34, 25, 12, 22, 11, 90, 1, 0, 100};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    cout<< "Size of array: " << n << endl;
+
+    cout << "Unsorted array: \n";
+    print(arr, n);
+
+    cout << "Sorted array: \n";
+    insertionSort(arr, n);
+    print(arr, n);
+
+    
+    return 0;
+}
